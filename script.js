bb885b4ed7b1e6258f0be363a57ad254e82c25c5
Adam A <adamaubry2362@gmail.com>
2022-05-01 04:23:20 -0500

    Finished single round. step 4

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 
-function computerSelection(){
-    let options = ['rock','paper','scissors']
-    return options[Math.floor(Math.random() * options.length)];
-}
+function computerPlay(){
+     let options = ['rock','paper','scissors']
+     return options[Math.floor(Math.random() * options.length)];
+ }
+ 
+ 
+ function userPlay(){
+     const input = prompt('Rock, Paper, Scissors');
+     const newInput = input.toLowerCase();
+     if (newInput === 'rock'){
+         return 'rock';
+     }else if(newInput === 'paper'){
+         return 'paper';
+     }else{
+         return 'scissors';
+     }
+ }
+ 
+ function playRound(playerSelection, computerSelection) {
+     if(playerSelection === 'rock' && computerSelection === 'paper'){
+         return ("You Lose! Paper beats Rock");
+     }if(playerSelection === 'paper' && computerSelection === 'scissors'){
+         return ("You Lose! Scissors beats Paper");
+     }if(playerSelection === 'scissors' && computerSelection === 'rock'){
+         return ("You Lose! Rock beats Scissors");
+     }if(playerSelection === 'paper' && computerSelection === 'rock'){
+         return  ("You Win! Paper beats Rock");
+     }if(playerSelection === 'scissors' && computerSelection === 'paper'){
+         return ("You Win! Scissors beats Paper");
+     }if(playerSelection === 'rock' && computerSelection === 'scissors'){
+         return ("You Win! Rock beats Rock");
+     }if(playerSelection ===  computerSelection){
+         return "Its a draw";
+     }
+ }
 
-function playerSelection(){
-    let input = prompt('Rock, Paper, Scissors');
-    let newInput = input.toLowerCase();
-    if (newInput === 'rock'){
-        return console.log('rock');
-    }else if(newInput === 'paper'){
-        return console.log('paper');
-    }else{
-        return console.log('scissors')
-    }
-}
-
-playerSelection()
+let userChoice = userPlay();
+let computerSelection = computerPlay();
+let result = playRound(userChoice, computerSelection)
+console.log("user's choice", userChoice);
+console.log("computer's choice", computerSelection);
+console.log("Result is", result);
