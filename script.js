0f4e53cd4f545493b5eae597ddba344c3dad8cb8
Adam A <adamaubry2362@gmail.com>
2022-05-01 23:35:30 -0500

    Finsihed javascript for game output  in console.log

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+//Initial score start
 let compterScore = 0;
 let playerScore = 0
 let drawScore = 0
+
+//Have the computer generate a random pick(rock paper or scissors)
 function computerPlay(){
      let options = ['rock','paper','scissors']
      return options[Math.floor(Math.random() * options.length)];
  }
 
+ //Determine who wins the round when comapred 
+ //Also add point to winner
  function playRound(playerSelection, computerSelection) {
      if(playerSelection === 'rock' && computerSelection === 'paper'){
          compterScore++;
@@ -31,21 +36,41 @@ function computerPlay(){
      }
 
  }
-
+//Variables for who wins the round
  let playerWinsRound = "You Win this Round!"
  let computerWinsRound = "You Lose this Round!"
  let draw = "Its a draw"
 
+ //The number of rounds played with the output included 
+ //Determines the winner of the 5 rounds played.
  function game(){
     for(let i = 0; i <5; i++){
         let userChoice = prompt('Rock, Paper, Scissors').toLowerCase();
         let computerSelection = computerPlay();
         let result = playRound(userChoice, computerSelection)
+        console.log("Round", i + 1)
         console.log("user's choice", userChoice);
         console.log("computer's choice", computerSelection);
         console.log("Result is", result);
-        console.log(compterScore, playerScore, drawScore)
+        console.log("Computers Score", compterScore);
+        console.log("Your score", playerScore);
+        console.log("Tie score", drawScore);
+        console.log("//////////////")
         console.log("//////////////")
     }
+    if(compterScore > playerScore && compterScore > drawScore){
+        console.log("Sorry, the computer won")
+    }if(playerScore > compterScore && playerScore > drawScore){
+        console.log("Congradulations! You won!")
+    }if(drawScore === playerScore && drawScore > compterScore){
+        console.log("Congradulations! You won!")
+    }if(drawScore === compterScore && drawScore > playerScore){
+        console.log("Sorry, the computer won );")
+    }if(drawScore > playerScore && playerScore > compterScore){
+        console.log("Congradulations! You won!")
+    }if(drawScore > compterScore && compterScore > playerScore){
+        console.log("Sorry, the computer won );")
+    }
  }
+ //starts the game
  game()
